5ee808dbdedea836adb3e748da312860dfea9c9d
farkhans <farkhansyawalharahap@gmail.com>
2022-11-16 05:36:36 +0700

    Tugas 8

--- counter_7/lib/data.dart
+++ counter_7/lib/data.dart
@@ -13,7 +13,6 @@ class MyData extends StatefulWidget {
 }
 
 class _MyDataState extends State<MyData> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,84 +20,67 @@ class _MyDataState extends State<MyData> {
         title: Text(widget.title),
       ),
       drawer: const MyDrawer(),
-      body: ListView.separated(
+      body: ListView.builder(
         itemBuilder: ((context, index) {
-          return ListTile(
-            title: Container(
-              alignment: Alignment.centerLeft,
-              child: Row(
-                children: [
-                  Icon(
-                    Icons.attach_money,
-                    color: Budget.listBudget[index].jenis == 'Pemasukan'
-                        ? Colors.green
-                        : Colors.red,
-                    size: 80,
-                  ),
-                  const SizedBox(
-                    height: 10,
-                    width: 10,
-                  ),
-                  Column(
+          Budget budget = Budget.listBudget[index];
+          return Container (
+            decoration: BoxDecoration(
+              color: Colors.white70,
+              border: Border.all(
+                color: Colors.black54,
+              ),
+              borderRadius: BorderRadius.circular(5.0),
+            ),
+            margin: const EdgeInsets.only(
+              top: 10.0,
+              left: 10.0,
+              right: 10.0,
+            ),
+            padding: const EdgeInsets.all(4.0),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.attach_money,
+                  size: 60,
+                  color:
+                      budget.jenis == 'Pemasukan' ? Colors.green : Colors.red,
+                ),
+                const SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 4.0,
-                          bottom: 4.0,
-                          left: 4.0,
-                        ),
-                        child: Text(
-                          Budget.listBudget[index].judul,
-                          style: const TextStyle(
-                            fontSize: 14,
+                      Row (
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            budget.judul,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 18,
+                            ),  
                           ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text(
-                          Budget.listBudget[index].nominal,
-                          style: const TextStyle(
-                            fontSize: 14,
+                          Text(
+                            DateFormat.yMMMEd().format(budget.tanggal),
+                            style: const TextStyle(
+                              fontStyle: FontStyle.italic
+                            ),
                           ),
-                        ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 4.0,
-                          left: 4.0,
-                          bottom: 4.0,
-                        ),
-                        child: Text(
-                          Budget.listBudget[index].jenis,
-                          style: const TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          top: 4.0,
-                          left: 4.0,
-                        ),
-                        child: Text(
-                          DateFormat.yMMMEd().format(Budget.listBudget[index].tanggal),
-                          style: const TextStyle(
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
+                      const SizedBox(height: 5),
+                      Text(budget.nominal),
+                      const SizedBox(height: 5),
+                      Text(budget.jenis)
                     ],
                   ),
-                ],
-              ),
+                )
+              ],
             ),
           );
         }),
-        separatorBuilder: ((context, index) => const Divider(
-              height: 0,
-            )),
         itemCount: Budget.listBudget.length,
       ),
     );
